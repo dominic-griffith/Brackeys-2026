cce44f04e2572fedc26e7170b9d9b7938dac44d9
dominic-griffith <65990043+dominic-griffith@users.noreply.github.com>
2026-08-26 15:08:41 -0700

    Name Change, Script Progress
    - Changed Naming convention for Input Action Map. Focus for "e" press and Interact for "mouse1"
    - Player Interaction & Draggable Scripts Inprogress

---
+++ Assets/Scripts/Player/Draggable.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class Draggable : MonoBehaviour
+{
+    [SerializeField] private bool _canDrag = true;
+
+    // Use these to:
+    // Add SFX & VFX for when the player starts dragging this object.
+    // Add Check that item in correct location on drag end
+    [SerializeField] private UnityEvent _onDragStarted;
+    [SerializeField] private UnityEvent _onDragEnded;
+
+    private Vector2 _pointerOffset;
+
+    public void BeginDrag(Vector2 pointerPosition)
+    {
+        if (!_canDrag)
+            return;
+
+        // Remember the distance between the pointer and object center.
+        _pointerOffset = (Vector2)transform.position - pointerPosition;
+
+        _onDragStarted?.Invoke();
+    }
+
+    public void Drag(Vector2 pointerPosition)
+    {
+        if (!_canDrag)
+            return;
+
+        Vector3 newPosition = new Vector3(pointerPosition.x + _pointerOffset.x, pointerPosition.y + _pointerOffset.y, transform.position.z);
+
+        transform.position = newPosition;
+    }
+
+    public void EndDrag()
+    {
+        if (!_canDrag)
+            return;
+
+        _onDragEnded?.Invoke();
+    }
+
+    public void SetCanDrag(bool canDrag)
+    {
+        _canDrag = canDrag;
+    }
+
+}
