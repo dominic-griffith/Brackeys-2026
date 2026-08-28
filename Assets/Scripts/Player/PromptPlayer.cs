7d071a9d863796ea16f016796345d6645494dabf
Airworthy0 <98292430+Airworthy0@users.noreply.github.com>
2026-08-28 04:59:41 -0500

    UI Prompt over interactable objects
    - Made prompt be automatically set to disabled on game start.
    - Shows prompt over object only when player gets into certain range.

--- Assets/Scripts/Player/PromptPlayer.cs
+++ Assets/Scripts/Player/PromptPlayer.cs
@@ -8,13 +8,15 @@ public class PromptPlayer : MonoBehaviour
 
     private void Awake()
     {
-        
+        // Sets the _promptUI text to be disabled on initialization.
+        _promptUI.SetActive(false);
     }
 
     public void ShowPrompt(bool show)
     {
         if (_promptUI != null)
         {
+            // Shows Prompt button when player is in-range of trigger collider
             _promptUI.SetActive(show);
         }
     }
